5e9a341b2d33de250eb80b13a54b4e9610b99e3d
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-12-31 11:20:06 +0100

    Validation in generateQR

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
 
 function generateQR(){
-    qrImage.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    let text = qrText.value.trim();
+
+    if(text === ""){
+        alert("Please enter a valid text or URL!");
+        return;
+    }
+
+    qrImage.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + text;
 }
