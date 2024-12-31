df153d5830b06c282db004ccf57fda948d03ca9b
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-12-31 11:56:15 +0100

    Validation with animation!

--- script.js
+++ script.js
@@ -2,16 +2,20 @@ let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
 
-function generateQR(){
+function generateQR() {
     let text = qrText.value.trim();
 
-    if(text === ""){
-        alert("Please enter a valid text or URL!");
-        return;
-    }
-    
-    // font:  https://goqr.me/api/
-    qrImage.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + text;
+    if (text.length > 0) {
+        // font:  https://goqr.me/api/
+        qrImage.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + text;
 
-    imgBox.classList.add("show-img");
+        imgBox.classList.add("show-img");
+
+    } else {
+        qrText.classList.add("error");
+        setTimeout(()=>{
+            qrText.classList.remove("error");
+        }, 1500);
+    }
+
 }
